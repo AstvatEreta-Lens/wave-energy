ff9b3080565e0c4448fed4e0563b9044fe83565b
Ahmad Al <108104535+AstvatEreta-Lens@users.noreply.github.com>
2023-02-12 13:33:58 +0700

    Update core_machine.py

--- core/core_machine.py
+++ core/core_machine.py
@@ -2,43 +2,35 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.sidebar.success("Nanti Fitur kita masukan sini")
-st.title('Simulation Prediction Wave Energy Potential with Machine Learning')
-st.text('LKTC Tanker Goes to Bali\n')
+# Title
+st.title('Counting App Ocean Wave Power')
+st.text('By Student of Pertamina University\n')
+
+# Sidebar
+with st.sidebar:
+    side_title = st.success("Input the Parameters")
+    v_arus_laut = st.number_input("Kecepatan Arus laut (m/s)")
+    # gaya hidrodinamik
+    kerapatan_massa = st.slider("Kerapatan Massa Ai (kg/m^3)", 1000, 1100 )
+    A_luas_penampang = st.number_input("Luas Penampang(m^2)")
+    Efisiensi_turbin = st.slider("Efisiensi(%)", 0.0, 100.0)
+ 
+# Operation
+daya_listrik_arus = 0.5*kerapatan_massa*A_luas_penampang*abs(v_arus_laut**3)*(Efisiensi_turbin/100)
 
 # metric hasil
 mtr1, mtr2, mtr3  = st.columns(3)
-mtr1.metric("Potensi", "100 MW", "24 MW")
-mtr2.metric("Kecepatan Gelombang", "10 mps", "-2 mph")
-mtr3.metric("Kecapatan angin", "32 mps", "-9 mph")
+mtr1.metric("Daya", daya_listrik_arus)
+mtr2.metric("Efisiensi", Efisiensi_turbin )
+mtr3.metric("Kecapatan Arus", v_arus_laut )
 
-# contoh raw data/data set
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# Save data
+save_value = st.button("save")
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+#chart
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! nanti kita cari datasetnya")
 
-if st.checkbox('Contoh dataset'):
-    st.subheader('Raw data')
-    st.write(data)
 
-st.subheader('Testing histogram')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Modelling Mapping %s:00' % hour_to_filter)
-st.map(filtered_data)
+
